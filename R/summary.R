77a6afa27498aaf9b86627af15dd67bb2c8475ed
Andy Teucher <andy.teucher@gmail.com>
2024-02-14 10:31:54 -0800

    summary.sample_sessions()

--- R/summary.R
+++ R/summary.R
@@ -12,14 +12,14 @@
 
 #' @export
 summary.sample_station_info <- function(object, ...) {
-  cli::cat_boxx(object$study_area_name[1], "Study Area")
+  cli::cat_boxx(object$study_area_name[1], "Study Area", col = "lightgreen")
   cli::cli_alert_info(c(
     "{.val {length(unique(object$sample_station_label))}}",
     " sample station{?s} in {.val {nrow(object)}} location{?s}."
   ))
   if ("spatial_outlier" %in% names(object)) {
     n_outliers <- sum(as.numeric(object$spatial_outlier))
-    cli::cli_alert_warning(
+    cli::cli_alert_danger(
       "Detected {.val {n_outliers}} potential spatial outlier{?s}."
     )
   }
@@ -34,4 +34,29 @@ summary.sample_station_info <- function(object, ...) {
     padding = 0
   )
   cli::cli_alert_info("Set dates: Between {.val {range(object$set_date)}}")
+  cli::cli_alert_warning("Run {.code map_stations(object)} to view stations on a map.")
+}
+
+#' @export
+summary.sample_sessions <- function(object, ...) {
+  cli::cat_boxx(object$study_area_name[1], "Study Area", col = "lightgreen")
+  cli::cli_alert_info(c(
+    "{.val {length(unique(object$sample_station_label))}}",
+    " sample station{?s} in {.val {length(unique(object$deployment_label))}} deployments{?s}."
+  ))
+  dep_dur_range <- round(range(object$sample_duration_days, na.rm = TRUE))
+  n_invalid_samples <- sum(!object$sample_duration_valid)
+  cli::cli_alert_info(
+    "Sample sessions lengths range between {.val {dep_dur_range}} days."
+  )
+  if (sum(n_invalid_samples) > 0) {
+    cli::cli_alert_danger(
+    "There {?is/are} {.val {n_invalid_samples}} invalid sample session{?s}."
+    )
+  }
+  n_photos_range <- range(object$number_of_photos, na.rm = TRUE)
+  cli::cli_alert_info(c(
+    "There are {.val {sum(object$number_of_photos)}} images. ",
+    "Photos per session range betwen {.val {n_photos_range}}."
+  ))
 }
