23bd703cfbd4337f90b9a941e48a9a72be1c2abe
Andy Teucher <andy.teucher@gmail.com>
2024-03-05 11:54:34 -0800

    Write data: * Image data as a csv: write_image_data() * Fill entire SPI template: fill_spi_template() * Fill single SPI sheet: write_to_spi_sheet() (generic with 4 methods)

--- R/write-data.R
+++ R/write-data.R
@@ -1,19 +1,13 @@
-#' Write image data to a csv or SPI submission template
+#' Write image data to a csv file
 #'
-#' if output `file` specified is a csv file, this will output all of the image
-#' data to a single csv file. If it is a `xls*` file, it will populate the
-#' `Sequence Image Data` tab in a new SPI submission sheet, using a template.
+#' This is a very thin wrapper around [readr::write_csv()]. You can use that
+#' function directly for more control
+#'
 #'
 #' @param image_data image data object.
-#' @param file path to the output file
-#' @param ... extra columns in `image_data` to write out. Must be paired column
-#'   names in the form \code{`Destination Column` = data_column}. If the left-hand side
-#'   is a syntactically valid name it can be provided as-is, but if it has spaces in it
-#'   it must be wrapped in backticks or quotes. See examples.
+#' @param file path to the output csv file
 #' @param na How should missing values be written. Default empty (`""`)
-#' @param template SPI submission template to use. Default is included in the
-#'   package, accessed by
-#'   `system.file("GENERIC_wildlife_camera_template_v2021.xlsm", package = "bccamtrap")`
+#' @param ... other parameters passed on to [readr::write_csv()]
 #'
 #' @return input `image_data`, invisibly
 #' @export
@@ -24,32 +18,128 @@
 #' write_image_data(image_data, "my_spi_submission.xlsx", Surveyor = surveyor,
 #'    `Survey Observation Photos` = file)
 #' }
-write_image_data <- function(image_data, file, ..., na = "", template = system.file("GENERIC_wildlife_camera_template_v2021.xlsm", package = "bccamtrap")) {
+write_image_data <- function(image_data, file, na = "", ...) {
   check_image_data(image_data)
   check_name(file)
 
-  ext <- tools::file_ext(file)
+  readr::write_csv(image_data, file, na = na, ...)
+}
 
-  if (!ext %in% c("csv", "xls", "xlsx", "xlsm")) {
-    cli::cli_abort("file must be a csv or Excel file name")
+#' Write all data to a new SPI submission file
+#'
+#' This will write only the default columns for each of the tabs in the SPI
+#' template. For more fine-grained control, use [write_to_spi_sheet()] on each
+#' of `sample_station_info`, `camera_info`, `cam_setup_checks`, `image_data`.
+#'
+#' @param sample_station_info object of class `sample_station_info`
+#' @param camera_info object of class `camera_info`
+#' @param camera_setup_checks object of class `camera_setup_checks`
+#' @param image_data object of class `image_data`
+#' @param file path to the output csv file
+#'
+#' @return path to the output `file`
+#' @export
+fill_spi_template <- function(sample_station_info,
+                              camera_info,
+                              camera_setup_checks,
+                              image_data,
+                              file,
+                              template = system.file("GENERIC_wildlife_camera_template_v2021.xlsm", package = "bccamtrap")) {
+
+  check_sample_station_info(sample_station_info)
+  check_camera_info(camera_info)
+  check_cam_setup_checks(camera_setup_checks)
+  check_image_data(image_data)
+  check_name(file)
+
+  if (!tools::file_ext(file) %in% c("xls", "xlsx", "xlsm")) {
+    cli::cli_abort("file must be an Excel file name")
   }
 
-  if (ext == "csv") {
-    return(
-      readr::write_csv(image_data, file, na = na, ...)
-    )
-  }
+  lapply(
+    list(sample_station_info, camera_info, camera_setup_checks, image_data),
+    function(x) {
+      write_to_spi_sheet(x, output_file = file, template = template)
+    }
+  )
+  invisible(file)
+}
 
-  write_to_spi_sheet(
-    image_data,
-    output_file = file,
-    template = template,
-    sheet = "Sequence Image Data",
-    ...
+#' Write a single data frame to SPI submission template
+#'
+#' @param x object of class `sample_station_info`, `camera_info`, `cam_setup_checks`, or `image_data`
+#' @param file path to the output file (`.xls*`)
+#' @param ... extra columns in `x` to write out. Must be paired column
+#'   names in the form \code{`Destination Column` = data_column}. If the left-hand side
+#'   is a syntactically valid name it can be provided as-is, but if it has spaces in it
+#'   it must be wrapped in backticks or quotes. See examples.
+#' @param template SPI submission template to use. Default is included in the
+#'   package, accessed by
+#'   `system.file("GENERIC_wildlife_camera_template_v2021.xlsm", package = "bccamtrap")`
+#'
+#' @return input `x`, invisibly
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' write_to_spi_sheet(camera_setup_checks_data, "my_spi_submission.xlsx")
+#' write_to_spi_sheet(image_data, "my_spi_submission.xlsx", Surveyor = surveyor,
+#'    `Survey Observation Photos` = file)
+#' }
+write_to_spi_sheet <- function(x, file, ..., template) {
+  UseMethod("write_to_spi_sheet")
+}
+
+#' @export
+write_to_spi_sheet.default <- function(x, file, ..., template) {
+  cli::cli_abort("No method for object of class {class(x)}")
+}
+
+#' @export
+write_to_spi_sheet.sample_station_info <- function(x, file, ..., template) {
+  write_to_spi_sheet_impl_(
+    x,
+    file,
+    sheet = "Sample Station Information",
+    ...,
+    template = template
   )
 }
 
-write_to_spi_sheet <- function(x, output_file, template, sheet, ...) {
+#' @export
+write_to_spi_sheet.camera_info <- function(x, file, ..., template) {
+  write_to_spi_sheet_impl_(
+    x,
+    file,
+    sheet = "Camera Information",
+    ...,
+    template = template
+  )
+}
+
+#' @export
+write_to_spi_sheet.cam_setup_checks <- function(x, file, ..., template) {
+  write_to_spi_sheet_impl_(
+    x,
+    file,
+    sheet = "Camera Setup and Checks",
+    ...,
+    template = template
+  )
+}
+
+#' @export
+write_to_spi_sheet.image_data <- function(x, file, ..., template) {
+  write_to_spi_sheet_impl_(
+    x,
+    file,
+    sheet = "Sequence Image Data",
+    ...,
+    template = template
+  )
+}
+
+write_to_spi_sheet_impl_ <- function(x, output_file, sheet, ..., template = system.file("GENERIC_wildlife_camera_template_v2021.xlsm", package = "bccamtrap")) {
   wb <- openxlsx2::wb_load(template)
 
   sheets <- wb$get_sheet_names()
@@ -99,13 +189,32 @@ write_to_spi_sheet <- function(x, output_file, template, sheet, ...) {
 get_default_columns <- function(x, sheet) {
   switch(
     sheet,
+    "Sample Station Information" = list(
+      `Study Area Name` = x$study_area_name,
+      `Sample Station Label` = x$sample_station_label,
+      `Longitude Sample Station (DD)` = x$longitude_sample_station_dd,
+      `Latitude Sample Station (DD)` = x$latitude_sample_station_dd
+    ),
+    "Camera Information" = list(
+      `Study Area Name` = x$study_area_name,
+      `Camera Label` = x$camera_label,
+      `Longitude Sample Station (DD)` = x$longitude_sample_station_dd,
+      `Latitude Sample Station (DD)` = x$latitude_sample_station_dd
+    ),
+    "Camera Setup and Checks" = list(
+      `Study Area Name` = x$study_area_name,
+      `Camera Label` = x$camera_label,
+      `Date` = format(x$samplinge_start_date, "%d-%b-%Y"),
+      `Visit End Date` = format(x$sampling_end, "%d-%b-%Y")
+    ),
     "Sequence Image Data" = list(
       `Study Area Name` = x$study_area_name,
-      `Camera Label` = x$sample_station_label,
+      `Camera Label` = x$sample_station_label, # TODO: Join cam setup and checks to get camera label
       `Detection Date` = format(x$date_time, "%d-%b-%Y"),
       `Detection Time` = format(x$date_time, "%H:%M:%S"),
       `Species Code` = x$species,
       `Count` = x$total_count_episode
-    )
+    ),
+    cli::cli_abort("Invalid sheet name: {sheet}")
   )
 }
