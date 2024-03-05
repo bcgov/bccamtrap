23044f07ff9057dfdc0703134335b012b8da4d8a
Andy Teucher <andy.teucher@gmail.com>
2024-03-05 14:32:26 -0800

    Hide names

--- tests/testthat/test-summary.R
+++ tests/testthat/test-summary.R
@@ -1,6 +1,6 @@
 test_that("summary.sample_stations works", {
-  ss1 <- read_sample_station_info(test_meta_file1)
-  ss2 <- read_sample_station_info(test_meta_file2)
+  ss1 <- hide_names(read_sample_station_info(test_meta_file1))
+  ss2 <- hide_names(read_sample_station_info(test_meta_file2))
 
   expect_snapshot(summary(ss1))
   expect_snapshot(summary(ss2))
@@ -13,16 +13,16 @@ test_that("summary.sample_stations works", {
 })
 
 test_that("summary.deployments works", {
-  sessions1 <- make_deployments(test_meta_file1)
-  sessions2 <- make_deployments(test_meta_file2)
+  sessions1 <- hide_names(make_deployments(test_meta_file1))
+  sessions2 <- hide_names(make_deployments(test_meta_file2))
 
   expect_snapshot(summary(sessions1))
   expect_snapshot(summary(sessions2))
 })
 
 test_that("summary.image_data works", {
-  id1 <- read_image_data(test_dir_1)
-  id2 <- read_image_data(test_dir_2)
+  id1 <- hide_names(read_image_data(test_dir_1))
+  id2 <- hide_names(read_image_data(test_dir_2))
 
   expect_snapshot(summary(id1))
   expect_snapshot(summary(id2))
