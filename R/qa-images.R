0b7f9f2fb9accefbbf7e7897ef5a62525e6e4fb6
Andy Teucher <andy.teucher@gmail.com>
2024-03-12 14:07:25 -0700

    Only return QA columns with issues

--- R/qa-images.R
+++ R/qa-images.R
@@ -54,6 +54,13 @@ qa_image_data <- function(image_data, exclude_human_use = TRUE, check_snow = TRU
     image_data,
     dplyr::if_any(dplyr::starts_with("QA"))
   )
+
+  # and drop QA columns with no issues
+  dplyr::select(
+    out,
+    dplyr::everything(),
+    -dplyr::starts_with("QA_"), dplyr::where(~ is.logical(.x) && !any(.x))
+  )
 }
 
 find_blanks <- function(x) {
