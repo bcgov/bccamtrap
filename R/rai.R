556f2bb231631782829257f7aa7391660fdde3e2
Andy Teucher <andy.teucher@gmail.com>
2024-03-22 10:46:25 -0700

    Flesh out rai

--- R/rai.R
+++ R/rai.R
@@ -1,18 +1,74 @@
-rai <- function(sessions, image_data, deployment_label = NULL, species = NULL, sample_start_date = NULL, sample_end_date = NULL) {
+#' Calculate Relative Activity Index (RAI)
+#'
+#' Calculate the RAI
+#'
+#' @param sample_sessions A data.frame of `sample_sessions`, from
+#'   [make_sample_sessions()]
+#' @inheritParams qa_image_data
+#' @param deployment_label One or more deployment labels to select
+#' @param species One or more species as a character vector. Default `NULL` to
+#'   calculate RAI for each species detected.
+#' @param by_deployment_label Should an RAI be calculated for each deployment
+#'   label (`TRUE`, default), or one RAI for all deployment labels (`FALSE`)
+#' @param by_species Should an RAI be calculated for each species (`TRUE`,
+#'   default), or one RAI for all species (`FALSE`)
+#' @param sample_start_date a custom start date. Note that this will apply the
+#'   same start date to all deployments/sessions in the data, so setting this is
+#'   most useful when using a single deployment by setting `deployment_label` to
+#'   a single value.
+#' @param sample_end_date a custom end date. Note that this will apply the same
+#'   start date to all deployments/sessions in the data, so setting this is most
+#'   useful when using a single deployment by setting `deployment_label` to a
+#'   single value.
+#'
+#' @return a data.frame of RAI, by deployment label and species (if `by_species = TRUE`)
+#' @export
+#'
+#' @examples
+rai <- function(sample_sessions,
+                image_data,
+                deployment_label = NULL,
+                species = NULL,
+                by_deployment_label = TRUE,
+                by_species = TRUE,
+                sample_start_date = NULL,
+                sample_end_date = NULL) {
+
+  check_sample_sessions(sample_sessions)
+  check_image_data(image_data)
+
   dat <- dplyr::right_join(
-    sessions,
+    sample_sessions,
     image_data,
     by = c("sample_station_label", "deployment_label")
   )
 
+  dat$sample_end_date <- dat$max_tl_date
+
+  if (!is.null(sample_start_date)) {
+    dat <- dplyr::filter(.data$date_time >= as.POSIXct(sample_start_date))
+    dat$sample_start_date <- sample_start_date
+  }
+
+  if (!is.null(sample_end_date)) {
+    dat <- dplyr::filter(.data$date_time <= as.POSIXct(sample_end_date))
+    dat$sample_end_date <- sample_end_date
+  }
+
   dat <- dplyr::group_by(
     dat,
-    .data$deployment_label,
     .data$sample_start_date,
-    sample_end_date = .data$max_tl_date,
-    .data$sample_period_length,
-    .data$species
+    .data$sample_end_date,
+    .data$sample_period_length
   )
+
+  if (isTRUE(by_species)) {
+    dat <- dplyr::group_by(dat, .data$species, .add = TRUE)
+  }
+
+    if (isTRUE(by_deployment_label)) {
+    dat <- dplyr::group_by(dat, .data$deployment_label, .add = TRUE)
+  }
 
   dat <- dplyr::filter(dat, !is.na(.data$species))
 
