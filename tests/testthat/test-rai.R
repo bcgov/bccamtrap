799204c723a34b3529b4223ca21d9fbe63551c24
Andy Teucher <andy.teucher@gmail.com>
2024-03-25 16:33:03 -0700

    Use numeric instead of snapshot tests for rolling averages

--- tests/testthat/test-rai.R
+++ tests/testthat/test-rai.R
@@ -1,26 +1,26 @@
 test_that("sample_rai works", {
-    imgs1 <- read_image_data(test_dir_1)
-    imgs2 <- read_image_data(test_dir_2)
+  imgs1 <- read_image_data(test_dir_1)
+  imgs2 <- read_image_data(test_dir_2)
 
-    expect_snapshot(sample_rai(imgs1))
-    expect_snapshot(sample_rai(imgs1, deployment_label = "A4_20230517"))
-    expect_snapshot(sample_rai(imgs1, species = "Roosevelt Elk"))
-    expect_snapshot(sample_rai(imgs1, by_deployment = FALSE, by_species = FALSE))
-    expect_snapshot(sample_rai(imgs1, by_deployment = FALSE))
-    expect_snapshot(sample_rai(imgs1, by_species = FALSE))
-    expect_snapshot(sample_rai(
-      imgs1,
-      sample_start_date = "2022-12-16",
-      sample_end_date = "2023-02-05"
-    ))
-    expect_snapshot(sample_rai(
-      imgs2,
-      deployment_label = "19_1_20230605",
-      species = "Roosevelt Elk",
-      sample_start_date = "2022-12-16",
-      sample_end_date = "2023-02-05"
-    ))
-  }
+  expect_snapshot(sample_rai(imgs1))
+  expect_snapshot(sample_rai(imgs1, deployment_label = "A4_20230517"))
+  expect_snapshot(sample_rai(imgs1, species = "Roosevelt Elk"))
+  expect_snapshot(sample_rai(imgs1, by_deployment = FALSE, by_species = FALSE))
+  expect_snapshot(sample_rai(imgs1, by_deployment = FALSE))
+  expect_snapshot(sample_rai(imgs1, by_species = FALSE))
+  expect_snapshot(sample_rai(
+    imgs1,
+    sample_start_date = "2022-12-16",
+    sample_end_date = "2023-02-05"
+  ))
+  expect_snapshot(sample_rai(
+    imgs2,
+    deployment_label = "19_1_20230605",
+    species = "Roosevelt Elk",
+    sample_start_date = "2022-12-16",
+    sample_end_date = "2023-02-05"
+  ))
+}
 )
 
 test_that("rai_by_time works", {
@@ -78,26 +78,48 @@ test_that("rai_by_time works", {
     ),
     dplyr::contains("roll")
   ))
-  expect_snapshot(rai_by_time(
+})
+
+test_that("rai_by_time with different agg functions", {
+  imgs2 <- read_image_data(test_dir_2)
+
+  rai_rolltest1 <- rai_by_time(
     imgs2,
     by = "date",
     species = "Roosevelt Elk",
     by_deployment = FALSE,
     roll = TRUE
-  )[["roll_rai"]])
-  expect_snapshot(rai_by_time(
+  )
+
+  expect_equal(
+    round(mean(rai_rolltest1$roll_rai, na.rm = TRUE), 3),
+    0.067
+  )
+
+  rai_rolltest2 <- rai_by_time(
     imgs2,
     by = "date",
     species = "Roosevelt Elk",
     by_deployment = FALSE,
     roll = TRUE
-  )[["roll_mean_max_snow"]])
-  expect_snapshot(rai_by_time(
+  )
+
+  expect_equal(
+    round(mean(rai_rolltest2$roll_mean_max_snow, na.rm = TRUE), 3),
+    4.485
+  )
+
+  rai_rolltest3 <- rai_by_time(
     imgs2,
     by = "date",
     species = "Roosevelt Elk",
     by_deployment = FALSE,
     roll = TRUE,
     snow_agg = mean
-  )[["roll_mean_mean_snow"]])
+  )
+
+  expect_equal(
+    round(mean(rai_rolltest3$roll_mean_mean_snow, na.rm = TRUE), 3),
+    1.697
+  )
 })
