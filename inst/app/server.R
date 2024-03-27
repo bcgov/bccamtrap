193b67a8379295e0cb4d9563f835635560148389
Andy Teucher <andy.teucher@gmail.com>
2024-03-27 12:19:35 -0700

    Add deployments and image QA data

--- inst/app/server.R
+++ inst/app/server.R
@@ -2,7 +2,6 @@
 function(input, output, session) {
 
   spi_meta <- reactiveValues()
-  image_data <- reactiveVal()
 
   observeEvent(
     input$spi_input$datapath, {
@@ -11,17 +10,19 @@ function(input, output, session) {
       qa_stations_spatial()
     spi_meta$camera_info <- read_camera_info(input$spi_input$datapath)
     spi_meta$cam_seup_checks <- read_cam_setup_checks(input$spi_input$datapath)
+    spi_meta$deployments <- make_deployments(input$spi_input$datapath)
   })
 
-  observe({
+  image_data <- reactive({
     req(input$image_input)
-    image_data(read_image_data(
+    read_image_data(
       # This is an escape hatch for check_template() in shiny - since the file
       # names assigned from fileInput() are random, we assign the csv files
       # names based on the original filenames, and use those to check the template
       stats::setNames(input$image_input$datapath, input$image_input$name)
-    ))
+    )
   })
+
 
   output$project_table <- renderTable(spi_meta$proj_info)
 
@@ -41,7 +42,7 @@ function(input, output, session) {
         site_description_comments ~ px(400),
         habitat_feature ~ px(150)
       ) %>%
-      data_color(columns = spatial_outlier, method = "factor", palette = c("red", "green")) %>%
+      data_color(columns = spatial_outlier, method = "factor", palette = c("#1E88E5", "#D81B60")) %>%
       opt_interactive(
         use_resizers = TRUE,
         use_compact_mode = TRUE
@@ -55,6 +56,63 @@ function(input, output, session) {
 
   output$ssi_summary <- renderText({
     capture.output(summary(req(spi_meta$sample_station_info)), type = "message")
+  })
+
+  output$deployments_table <- render_gt({
+    req(spi_meta$deployments) %>%
+      select(
+        -any_of(
+          c("wlrs_project_name", "study_area_name", "code")
+        ),
+        -starts_with("utm"),
+        -starts_with("easting"),
+        -starts_with("northing")
+      ) %>%
+      sf::st_drop_geometry() %>%
+      gt() %>%
+      cols_width(
+        general_sampling_comments ~ px(400),
+        site_description_comments ~ px(400),
+        habitat_feature ~ px(150)
+      ) %>%
+      opt_interactive(
+        use_resizers = TRUE,
+        use_compact_mode = TRUE
+      )
+  })
+
+  img_data_qa <- eventReactive(input$btn_qa_image_data, {
+    qa_image_data(
+      req(image_data()),
+      exclude_human_use = req(input$rb_exclude_human) == "Yes",
+      check_snow = req(input$rb_check_snow) == "Yes",
+      tl_time = format(req(input$tl_time), "%T")
+    )
+  })
+
+  output$img_qa_summary <- renderText({
+    qa_data <- req(img_data_qa())
+    c(
+    paste("Number of records with potential issues:", nrow(qa_data), "\n"),
+    "\nPotential Issues:\n",
+    paste0(grep("^QA_", names(qa_data), value = TRUE), collapse = "\n")
+    )
+  })
+
+  output$image_qa_table <- render_gt({
+    qa_data <- req(img_data_qa())
+    qa_data %>%
+      select(
+        -any_of(
+          c("wlrs_project_name", "study_area_name")
+        )
+      ) %>%
+        gt() %>%
+        data_color(columns = starts_with("QA_"), method = "factor", palette = c("#1E88E5", "#D81B60")) %>%
+        opt_interactive(
+          use_resizers = TRUE,
+          use_compact_mode = TRUE
+        )
   })
 
   output$spi_download <- downloadHandler(
@@ -75,5 +133,4 @@ function(input, output, session) {
       )
     }
   )
-
 }
--- inst/app/ui.R
+++ inst/app/ui.R
@@ -33,7 +33,8 @@ ui <- page_sidebar(
           accept = c(".csv"),
           multiple = TRUE
         )
-      )
+      ),
+      open = c("Metadata from SPI Worksheet", "TimeLapse Image Data")
     ),
     "Write to SPI worksheet",
     downloadButton(
@@ -64,10 +65,53 @@ ui <- page_sidebar(
       )
     ),
     nav_panel(
-      "Deployments"
+      "Deployments",
+      gt_output("deployments_table")
     ),
     nav_panel(
-      "Sample Sessions"
+      "Image Data QA",
+      layout_columns(
+        card(radioButtons(
+          "rb_exclude_human",
+          "Exclude Human Use?",
+          choices = c("Yes", "No"),
+          selected = "Yes",
+          inline = TRUE
+        )),
+        card(radioButtons(
+          "rb_check_snow",
+          "Check Snow Depth?",
+          choices = c("Yes", "No"),
+          selected = "Yes",
+          inline = TRUE
+        )),
+        card(sliderInput(
+          "tl_time",
+          "TimeLapse photo time",
+          min = as.POSIXct("2020-01-01 00:00:00", tz = "UTC"),
+          max = as.POSIXct("2020-01-01 23:59:59", tz = "UTC"),
+          value = as.POSIXct("2020-01-01 12:00:00", tz = "UTC"),
+          timeFormat = "%T",
+          timezone = "+0000"
+        )),
+        actionButton(
+          "btn_qa_image_data",
+          "Check!",
+          class = "btn-lg btn-success"
+        )
+      ),
+      card(
+        verbatimTextOutput("img_qa_summary")
+      ),
+      shinycssloaders::withSpinner(
+        gt_output("image_qa_table"),
+        type = 5,
+        color = "darkgrey"
+      )
+    ),
+    nav_panel(
+      "Sample Sessions",
+      gt_output("sessions_table")
     ),
     nav_panel(
       "Analysis data"
