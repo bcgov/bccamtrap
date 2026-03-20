90bfac37f3dd4dbe5180606ee457277ad50c63b2
Andy Teucher <andy.teucher@gmail.com>
2026-03-19 22:42:37 -0700

    Update scratch

--- inst/scratch-templates.R
+++ inst/scratch-templates.R
@@ -1,5 +1,6 @@
 # devtools::install_github("bcgov/bccamtrap")
 library(bccamtrap)
+library(dplyr)
 
 migration_data <- read_image_data(
   "inst/extdata/example-data/Example-input-CSV_migration-cams_Migration_v1.csv"
@@ -16,7 +17,23 @@ migration_data_qa <- qa_image_data(
   exclude_human_use = FALSE
 )
 
-migration_data_qa
+migration_map <- tribble(
+  ~sample_station_label , ~upstream ,
+  "Kay-A"               , "R to L"  ,
+  "Kay-B"               , "R to L"  ,
+  "UpperAdam-A"         , "L to R"  ,
+  "UpperAdam-B"         , "R to L"  ,
+  "SwampLake-A"         , "L to R"  ,
+  "SwampLaKe-B"         , "R to L"  ,
+  "SwampLake-B"         , "T to B"  ,
+  "MerrillLk-A"         , "L to R"  ,
+  "Salmon4-A"           , "R to L"  ,
+  "Salmon-A"            , "R to L"  ,
+  "Salmon-B"            , "L to R"  ,
+  "Memekay-A"           , "L to R"
+)
+
+collapse_migration_episodes(migration_data_qa, migration_map = migration_map)
 
 wallow_elk_data <- read_image_data(
   "inst/extdata/example-data/Example-input-CSV_wallow-cams_Elk_Template_Wallows_v1.csv"
@@ -33,7 +50,8 @@ wallow_elk_data_qa <- qa_image_data(
   exclude_human_use = FALSE
 )
 
-wallow_elk_data_qa
+
+collapse_wallow_episodes(wallow_elk_data)
 
 new_wallow_elk_data <- read_image_data(
   "inst/extdata/example-data/Example-input-CSV_wallow-cams_updated_approach.csv"
@@ -49,4 +67,5 @@ new_wallow_elk_data_qa <- qa_image_data(
   exclude_human_use = FALSE
 )
 
-new_wallow_elk_data_qa
+
+collapse_wallow_episodes(new_wallow_elk_data)
